575f27de0db6abaf7a97db53c5bf576f30b5a6d1
Sarp Cubukcuoglu <30846813+sinerjit@users.noreply.github.com>
2023-02-10 15:58:34 -0800

    some logs and changes

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# This is a True & False game
-
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
@@ -10,12 +8,10 @@ for question in question_data:
     question_text = question["text"]
     question_answer = question["answer"]
     new_question = Question(q_text=question_text, q_answer=question_answer)
-    # we don't need tags (q_text=, q_answer=) because positional parameters.
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
-while quiz.still_has_questions():  # if quiz still has question remaining:
-    # or quiz.answer_count():
+while quiz.still_has_questions():
     quiz.next_question()
 
 print("You've completed the quiz")
